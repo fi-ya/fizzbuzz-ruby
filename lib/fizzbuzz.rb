e7eda477cfc1a12985da6e4e717b3ca5e47e1358
fi-ya <safiaali@hotmail.com>
2022-02-21 14:01:05 +0000

    Fix bug: return value from fizzbuzz & test pass. Update README

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,36 +1,26 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-        puts 'FizzBuzz'
+    if number % 15 == 0
+        result = puts 'FizzBuzz'
+        return 'FizzBuzz'
     elsif number % 3 == 0
-        puts 'Fizz'
+        result = puts 'Fizz'
+        return 'Fizz'
     elsif number % 5 == 0 
-        puts 'Buzz' 
+        result = puts 'Buzz'
+        return 'Buzz'
     else
-        puts number
+        result = puts number
+        return number.to_s
     end
 end
 
-# fizzbuzz(3)
-# fizzbuzz(6)
-# fizzbuzz(5)
-# fizzbuzz(10)
-# fizzbuzz(15)
-# fizzbuzz(30)
-# fizzbuzz(31)
-# fizzbuzz(34)
-# fizzbuzz('15')
+fizzbuzz(3)
+fizzbuzz(6)
+fizzbuzz(5)
+fizzbuzz(10)
+fizzbuzz(15)
+fizzbuzz(30)
+fizzbuzz(31)
+fizzbuzz(34)
 
-# Loop nth times 
-# (1..n).each do |i|
-#     # add condition statment from above
-# end
 
-# Loop 1- 100 - no arg needed
-# 1.upto 100 do |number|
-     # add condition statment from above        
-# end
-
-# Loop 1- 100 - no arg needed
-# for number in 1..100
-     # add condition statment from above        
-# end
