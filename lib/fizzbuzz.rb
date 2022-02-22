a08337de2c2a36a22378bb49fcf59e65ff29ba1c
fi-ya <safiaali@hotmail.com>
2022-02-22 12:23:33 +0000

    feat: Add logic to print numbers 1-100

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,26 +1,14 @@
 def fizzbuzz(number)
     if number % 15 == 0
-        result = puts 'FizzBuzz'
-        return 'FizzBuzz'
+       'FizzBuzz'
     elsif number % 3 == 0
-        result = puts 'Fizz'
-        return 'Fizz'
+        'Fizz'
     elsif number % 5 == 0 
-        result = puts 'Buzz'
-        return 'Buzz'
+        'Buzz'
     else
-        result = puts number
-        return number.to_s
+        number.to_s
     end
 end
 
-fizzbuzz(3)
-fizzbuzz(6)
-fizzbuzz(5)
-fizzbuzz(10)
-fizzbuzz(15)
-fizzbuzz(30)
-fizzbuzz(31)
-fizzbuzz(34)
-
+(1..100).each {|number| puts "#{fizzbuzz(number)}"}
 
