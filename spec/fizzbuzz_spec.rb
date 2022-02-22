f0fd0643765f9416b3918107ea54f1dd035010f1
fi-ya <safiaali@hotmail.com>
2022-02-22 15:57:05 +0000

    refactor: group similar tests together in one it block & move edge cases test to the end
    Fix test description for tests that return a number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,34 +4,22 @@ describe 'fizzbuzz' do
 
     it 'returns "Fizz" when number divisible by 3' do
         expect(fizzbuzz(3)).to eq 'Fizz'
-    end   
-    
-    it 'return "Fizz" when passed number divisible 3' do
         expect(fizzbuzz(6)).to eq 'Fizz'
-    end  
-
+    end   
+     
     it 'return "Buzz" when passed number divisible 5' do
         expect(fizzbuzz(5)).to eq 'Buzz'
+        expect(fizzbuzz(10)).to eq 'Buzz'
     end   
 
-    it 'return "Buzz" when passed number divisible 5' do
-        expect(fizzbuzz(10)).to eq 'Buzz'
-    end 
+    it 'return "FizzBuzz" when passed number % 3 & 5' do
+        expect(fizzbuzz(15)).to eq 'FizzBuzz'
+        expect(fizzbuzz(30)).to eq 'FizzBuzz'
+    end   
 
     it 'return the number when passed number not divisible by 3 or 5' do
         expect(fizzbuzz(7)).to eq '7'
-    end   
-
-    it 'return the number when passed number not divisible by 3 or 5' do
         expect(fizzbuzz(19)).to eq '19'
-    end 
-
-    it 'return "Buzz" when passed number % 5' do
-        expect(fizzbuzz(15)).to eq 'FizzBuzz'
     end   
-
-    it 'return "Buzz" when passed number % 5' do
-        expect(fizzbuzz(30)).to eq 'FizzBuzz'
-    end
 
 end 
